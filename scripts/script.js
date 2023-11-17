1b6b9d4fbe8fe5a14012f6af02ae13c0f333128b
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-17 18:23:02 +0100

    Implement a function for the logic of the game

--- scripts/script.js
+++ scripts/script.js
@@ -33,3 +33,51 @@ function playRound(playerSelection, computerSelection) {
     return (rule[playerSelection][computerSelection]);
 }
 
+function game() {
+    let message;
+    let winner;
+    let result;
+    let placeHolder1;
+    let placeHolder2;
+    let placeHolder3;
+    let player = 0;
+    let computer = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        while (true) {
+            const computerSelection = getComputerChoice();
+            const playerSelection = getPlayerChoice();
+
+            result = playRound(playerSelection, computerSelection);
+            if (result === 0) {
+                console.log("Tie!");
+                console.log(`You (${player}) Vs Computer (${computer})`);
+                continue;
+            } 
+            
+            if (result === 1) {
+                player++;
+                placeHolder1 = "Won";
+                placeHolder2 = playerSelection;
+                placeHolder3 = computerSelection;
+            } else {
+                computer++;
+                placeHolder1 = "Lose";
+                placeHolder2 = computerSelection;
+                placeHolder3 = playerSelection;
+            }
+
+            message = `You ${placeHolder1}! ${placeHolder2} beats ${placeHolder3}`;
+            console.log(message);
+            console.log(`You (${player}) Vs Computer (${computer})`);
+            break;
+        }
+    }
+
+    console.log("Game Over!");
+    winner = player > computer ? "You" : "Computer";
+    console.log(`${winner} Won!`);
+    console.log(`You (${player}) Vs Computer (${computer})`);
+}
+
+game();
