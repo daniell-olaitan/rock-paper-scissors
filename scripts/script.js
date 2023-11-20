df13c978de69bb50dfc74698c7425260775eabbd
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-20 13:07:45 +0100

    Implement the web dynamics of the project
    Some of the methods for the console version have been changed to
    support the GUI methods.
    Only the gameplay has been implemented
    while the control (pause, rules, quit ...) buttons features have
    not been implemented

--- scripts/script.js
+++ scripts/script.js
@@ -5,79 +5,122 @@ function getComputerChoice() {
     return choices[choice];
 }
 
-function getPlayerChoice() {
-    const choices = ["rock", "scissors", "paper"];
-    
-    while (true) {
-        const choice = prompt("Enter your choice", "e.g. rock").toLowerCase();
-        
-        for (const c of choices) {
-            if (c === choice) {
-                return choice;
-            }
-        }
-    }
-}
-
 function playRound(playerSelection, computerSelection) {
     const rule = {
-        rock: {scissors: 1, paper: -1},
-        scissors: {rock: -1, paper: 1},
-        paper: {rock: 1, scissors: -1}
+        rock: { scissors: 1, paper: -1 },
+        scissors: { rock: -1, paper: 1 },
+        paper: { rock: 1, scissors: -1 }
     }
 
     if (playerSelection === computerSelection) {
         return 0;
     }
-    
-    return (rule[playerSelection][computerSelection]);
+
+    return rule[playerSelection][computerSelection];
 }
 
 function game() {
-    let message;
-    let winner;
-    let result;
-    let placeHolder1;
-    let placeHolder2;
-    let placeHolder3;
-    let player = 0;
-    let computer = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        while (true) {
-            const computerSelection = getComputerChoice();
-            const playerSelection = getPlayerChoice();
+    const round = document.querySelector('#round');
+    const compScore = document.querySelector('#comp-score');
+    const playerScore = document.querySelector('#player-score');
+    const gameArea = document.querySelector('#game-area');
+    const result = document.querySelector('#result');
+    const computerAvatar = document.querySelector('#cmp-avatar');
+    const playerAvatar = document.querySelector('#ply-avatar');
+    const instruction = document.querySelector('#instn');
+    const gameControl = document.querySelector('#ctrl');
 
-            result = playRound(playerSelection, computerSelection);
-            if (result === 0) {
-                console.log("Tie!");
-                console.log(`You (${player}) Vs Computer (${computer})`);
-                continue;
-            } 
-            
-            if (result === 1) {
-                player++;
-                placeHolder1 = "Won";
-                placeHolder2 = playerSelection;
-                placeHolder3 = computerSelection;
-            } else {
-                computer++;
-                placeHolder1 = "Lose";
-                placeHolder2 = computerSelection;
-                placeHolder3 = playerSelection;
+    let gameStart = true;
+    let pScore = 0;
+    let cScore = 0;
+    let roundDigit = 1;
+    let activeLoser;
+    let activeWinner;
+
+    gameArea.addEventListener('click', (event) => {
+        const computerSelection = getComputerChoice();
+        const computerChoice = document.querySelector('#' + computerSelection + '-cmp');
+
+        let playerSelection;
+        let roundResult;
+
+        if (['rock-ply', 'scissors-ply', 'paper-ply'].includes(event.target.id)) {
+            switch (event.target.id) {
+                case 'rock-ply':
+                    playerSelection = 'rock';
+                    break;
+
+                case 'scissors-ply':
+                    playerSelection = 'scissors';
+                    break;
+
+                case 'paper-ply':
+                    playerSelection = 'paper';
             }
 
-            message = `You ${placeHolder1}! ${placeHolder2} beats ${placeHolder3}`;
-            console.log(message);
-            console.log(`You (${player}) Vs Computer (${computer})`);
-            break;
-        }
-    }
+            roundResult = playRound(playerSelection, computerSelection);
+            if (roundResult === 0) {
+                result.textContent = "It's a Tie!";
+                activeLoser && activeLoser.classList.remove('active-lose');
+                activeWinner && activeWinner.classList.remove('active-win');
+            } else {
+                if (roundResult === 1) {
+                    result.textContent = "You Win!";
+                    activeLoser && activeLoser.classList.remove('active-lose');
+                    activeWinner && activeWinner.classList.remove('active-win');
+                    activeWinner = event.target.parentNode;
+                    activeLoser = computerChoice;
+                    pScore++;
+                    playerScore.textContent = `${pScore}`;
+                } else {
+                    result.textContent = "You Lose!";
+                    activeLoser && activeLoser.classList.remove('active-lose');
+                    activeWinner && activeWinner.classList.remove('active-win');
+                    activeWinner = computerChoice;
+                    activeLoser = event.target.parentNode;
+                    cScore++;
+                    compScore.textContent = `${cScore}`;
+                }
 
-    console.log("Game Over!");
-    winner = player > computer ? "You" : "Computer";
-    console.log(`${winner} Won!`);
-    console.log(`You (${player}) Vs Computer (${computer})`);
+                activeWinner.classList.add('active-win');
+                activeLoser.classList.add('active-lose');
+            }
+
+            if (cScore + pScore < 5) {
+                roundDigit++;
+                round.textContent = 'Round ' + roundDigit;
+            }
+
+            if (cScore + pScore === 5) {
+                const gameOverMessage = document.createElement('h1');
+                const playButton = document.createElement('button');
+                gameOverMessage.textContent = cScore > pScore ? 'You Lose!' : 'You Win';
+                playButton.textContent = "Play Again";
+                playButton.classList.add('play-again-btn');
+                activeLoser && activeLoser.classList.remove('active-lose');
+                activeWinner && activeWinner.classList.remove('active-win');
+                gameArea.innerHTML = '';
+                gameArea.classList.add('game-over');
+                gameArea.append(gameOverMessage, playButton);
+                instruction.textContent = '';
+                round.textContent = roundDigit + ' rounds played';
+
+
+                playButton.addEventListener('click', () => {
+                    cScore = 0;
+                    pScore = 0;
+                    compScore.textContent = `${cScore}`;
+                    playerScore.textContent = `${pScore}`;
+                    roundDigit = 1;
+                    round.textContent = 'Round ' + roundDigit;
+                    result.textContent = "Start!";
+                    gameArea.innerHTML = '';
+                    gameArea.append(computerAvatar, result, playerAvatar);
+                    instruction.textContent = 'Choose your weapon';
+                });
+            }
+        }
+    });
 }
 
-//game();
+game();
